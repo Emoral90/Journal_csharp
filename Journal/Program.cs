5a4e65ba74c9f64505a65fda53d1cc0c9b64a8ed
gasolina90 <moralerick90@gmail.com>
2023-01-20 15:59:49 -0700

    modified file

--- Journal/Program.cs
+++ Journal/Program.cs
@@ -1,5 +1,23 @@
-﻿public class Program{
+﻿using System;
+
+public class Program{
     static void Main(){
-        
+        string myChoice;
+
+        Journal journal = new Journal();
+        myChoice = journal.menu();
+
+        if (myChoice == "1"){
+            
+        }
     }
+
+    // static void validate_user_input(string input){
+    //     string input_str;
+    //     bool valid = string.TryParse(input, out input_str);
+
+    //     while (!valid){
+    //         Console.WriteLine("Invalid input. ");
+    //     }
+    // }
 }
