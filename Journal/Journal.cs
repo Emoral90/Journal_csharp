a34c1a291f0447bdf979741733b2dbf889e39e21
gasolina90 <moralerick90@gmail.com>
2023-01-20 15:59:38 -0700

    modified file

--- Journal/Journal.cs
+++ Journal/Journal.cs
@@ -1,8 +1,36 @@
-public class Journal{
+using System;
+using System.Collections.Generic;
 
-    string prompt_entry(){}
-    void display_entries(){}
-    string load_journal(){}
-    void save_journal(){}
-    string menu(){}
+public class Journal{
+    // Attributes
+    string user_input;
+
+    // Constructor
+    public Journal(){}
+
+    // Methods
+    public string prompt_entry(){}
+
+    // public void display_entries(){}
+
+    // public string load_journal(){}
+
+    // public void save_journal(){}
+
+    public string menu(){
+        while (user_input != "5"){
+            Console.WriteLine();
+            Console.WriteLine(" - - - - - - - Main Menu - - - - - - - -");
+            Console.WriteLine("========================================");
+            Console.WriteLine("1) Write a new entry");
+            Console.WriteLine("2) Display Journal");
+            Console.WriteLine("3) Load Journal from file");
+            Console.WriteLine("4) Save Journal to file");
+            Console.WriteLine("5) Quit");
+            Console.WriteLine();
+            
+            user_input = Console.ReadLine();
+        }
+        return user_input;
+    }
 }
