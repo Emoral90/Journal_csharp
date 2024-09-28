eb85e677714052c50ea277754e7b58a75b30ce24
Erick <moralerick90@gmail.com>
2024-09-28 15:49:07 -0700

    modified backend files

--- Journal/obj/Debug/net6.0/Journal.AssemblyInfo.cs
+++ Journal/obj/Debug/net6.0/Journal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Journal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3b55f63a83838fd2af35b0a6273aba07a8d7a17")]
 [assembly: System.Reflection.AssemblyProductAttribute("Journal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Journal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
